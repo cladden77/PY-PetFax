7490e3b09d949e81d06523d6b1717137960ac94d
Chris L <chris.ladden@gmail.com>
2022-09-27 11:46:30 -0500

    integrated postgres database with flask

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from . import models
+from flask_migrate import Migrate
 
-def create_app():
+def create_app(): 
     app = Flask(__name__)
+
+    # DB config
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Postgres@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)
 
     @app.route('/')
     def hello():
